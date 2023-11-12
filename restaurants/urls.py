2e6446f4ec40a7703c811bc4df5a3655adc4b845
Icaro Moro <icarosicchieri@usp.br>
2023-11-12 16:15:43 -0300

    Lista, busca e criação com forma e conteúdo errado

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 app_name = 'restaurants'
 urlpatterns = [
+    path('', views.list_movies, name='index'), # adicione esta linha
+    path('<int:movie_id>/', views.detail_movie, name='detail'), # adicione esta linha
+    path('search/', views.search_movies, name='search'), # adicione esta linha
+    path('create/', views.create_movie, name='create'), # adicione esta linha
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,41 @@
+from django.http import HttpResponse
+from .temp_data import movie_data
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def detail_movie(request, movie_id):
+    movie = movie_data[movie_id - 1]
+    return HttpResponse(
+        f'Detalhes do filme {movie["name"]} ({movie["release_year"]})')
+
+def list_movies(request):
+    context = {"movie_list": movie_data}
+    return render(request, 'restaurants/index.html', context)
+
+def detail_movie(request, movie_id):
+    context = {'movie': movie_data[movie_id - 1]}
+    return render(request, 'restaurants/detail.html', context)
+
+def search_movies(request):
+    context = {}
+    if request.GET.get('query', False):
+        context = {
+            "movie_list": [
+                m for m in movie_data
+                if request.GET['query'].lower() in m['name'].lower()
+            ]
+        }
+    return render(request, 'restaurants/search.html', context) # modifique esta linha
+
+def create_movie(request):
+    if request.method == 'POST':
+        movie_data.append({
+            'name': request.POST['name'],
+            'release_year': request.POST['release_year'],
+            'poster_url': request.POST['poster_url']
+        })
+        return HttpResponseRedirect(
+            reverse('restaurants:detail', args=(len(movie_data), )))
+    else:
+        return render(request, 'restaurants/create.html', {})
