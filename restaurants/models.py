70622e0681194d64aa1f486e58d1f8f73e6ca5ab
Icaro Moro <icarosicchieri@usp.br>
2023-11-13 01:21:55 -0300

    Relação categorias -> filmes

--- restaurants/models.py
+++ restaurants/models.py
@@ -19,3 +19,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'"{self.text}" - {self.author.username}'
+    
+class Category(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    content = content = models.TextField() 
+    posts = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return f'{self.name} by {self.author}'
+    
